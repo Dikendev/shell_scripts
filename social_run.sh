7d70dd60315812c9c46a90172da24e72920a12ce
Dikendev <diken.dev@gmail.com>
2024-08-14 02:28:29 -0300

    first commit

---
+++ social_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+url="https://mail.google.com/mail/u/0/#inbox"
+open -a "Google Chrome" $url
+echo "Open Gmail"
+
+githubUrl="https://github.com/"
+open -a "Google Chrome" $githubUrl
+echo "Open Github"
+
+linkedinUrl="https://www.linkedin.com/feed/"
+open -a "Google Chrome" $linkedinUrl
+echo "Open Linkedin""
---
+++ work.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source config.sh
+
+open -a "Google Chrome" $MIRO_URL
+echo "Open Miro"
+
+open -a "Google Chrome" $FIGMA_URL
+echo "Open Figma"
